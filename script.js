dc742d8d8749280dd9cd4f2ecd4ed449bac6fb56
Genteel Magpie <genteelmagpie@gmail.com>
2023-10-24 12:51:53 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// var firebase = window.firebase;
+
+// // Initialize Firebase
+// firebase.initializeApp({
+//   // Your Firebase configuration goes here
+// });
+
+// // Get the login button
+// var loginButton = document.getElementById('login-button');
+
+// // Add an event listener to the login button
+// loginButton.addEventListener('click', function() {
+//   // Get the email and password from the input fields
+//   var email = document.getElementById('email').value;
+//   var password = document.getElementById('password').value;
+
+//   // Sign in the user with email and password
+//   firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
+//     // The user has successfully signed in
+//     // Redirect the user to the main page of your application
+//     window.location.href = 'main.html';
+//   }).catch(function(error) {
+//     // The user failed to sign in
+//     // Show an error message to the user
+//     alert(error.message);
+//   });
+// });
